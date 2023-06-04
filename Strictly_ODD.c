cd3fab44b14f070f1061491f280fd254b4ac9ccc
mahitha2026 <119730326+mahitha2026@users.noreply.github.com>
2023-06-04 09:45:34 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             if(i%2==0)             {                 printf("False");                 return 0;             }         }     }     printf("True"); }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,36 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c;
+    int n,i;
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    int res=0;
     for(i=0;i<n;i++)
     {
         if(a[i]%2!=0)
         {
-            if(i%2!=0)
+            if(i%2==0)
             {
-                c=0;
-            }
-            else
-            {
-                c=1;
-                break;
+                printf("False");
+                return 0;
             }
         }
     }
-    if(c!=1)
-    {
-        printf("True");
-        
-    }
-    else
-    {
-        printf("False");
-    }
-}              
+    printf("True");
+}
